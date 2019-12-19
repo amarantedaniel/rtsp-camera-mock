da45c04367c49f37886d247037b69c723ef9cd04
Valmir Pretto <valmir.pretto@deepx.it>
2019-12-19 11:19:25 -0300

    Cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,36 @@
 use gstreamer_rtsp_server::prelude::*;
 
 use {
-  glib::{MainContext, MainLoop},
-  gstreamer_rtsp_server::{RTSPMediaFactory, RTSPServer},
+    glib::{MainContext, MainLoop},
+    gstreamer_rtsp_server::{RTSPMediaFactory, RTSPServer},
 };
 
 fn main() {
-  gstreamer::init().unwrap();
+    gstreamer::init().unwrap();
 
-  let media_factory = RTSPMediaFactory::new();
-  media_factory.set_launch("videotestsrc ! x264enc key-int-max=40 ! rtph264pay name=pay0 pt=96");
-  media_factory.set_shared(true);
+    let media_factory = RTSPMediaFactory::new();
+    media_factory.set_launch("videotestsrc ! x264enc key-int-max=40 ! rtph264pay name=pay0 pt=96");
+    media_factory.set_shared(true);
 
-  let main_context = MainContext::default();
+    let main_context = MainContext::default();
 
-  let server = RTSPServer::new();
-  let source_id = server.attach(Some(&main_context));
+    let server = RTSPServer::new();
+    let source_id = server.attach(Some(&main_context));
 
-  let end_point = "/stream";
+    let end_point = "/stream";
 
-  let mount_points = server.get_mount_points().unwrap();
-  mount_points.add_factory(end_point, &media_factory);
+    let mount_points = server.get_mount_points().unwrap();
+    mount_points.add_factory(end_point, &media_factory);
 
-  let main_loop = MainLoop::new(Some(&main_context), false);
+    let main_loop = MainLoop::new(Some(&main_context), false);
 
-  let address = server.get_address().unwrap();
-  let port = server.get_bound_port();
-  let location = format!("rtsp://{}:{}{}", address, port, end_point);
+    let address = server.get_address().unwrap();
+    let port = server.get_bound_port();
+    let location = format!("rtsp://{}:{}{}", address, port, end_point);
 
-  println!("Server available at {}", location);
+    println!("Server available at {}", location);
 
-  main_loop.run();
+    main_loop.run();
 
-  glib::source_remove(source_id);
+    glib::source_remove(source_id);
 }
